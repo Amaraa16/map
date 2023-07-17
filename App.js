620cce608ab9271cf0d615e22db78ed990df90dd
Жигжидсүрэн Амарсайхан <HP233023@M301-CC0140.local>
2023-07-17 15:44:55 +0800

    asd

--- App.js
+++ App.js
@@ -1,11 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
+import MapView, { Marker } from "react-native-maps";
+import React, { useState, useEffect } from "react";
+
+import * as Location from "expo-location";
 
 export default function App() {
+  const [location, setLocation] = useState(null);
+
+  console.log(location);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        alert("Permission to access location was denied");
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView provider="google" style={styles.map}>
+        {location && (
+          <Marker
+            coordinate={{
+              latitude: location.coords.latitude,
+              longitude: location.coords.longitude,
+            }}
+          >
+            <View
+              style={{
+                width: 50,
+                height: 50,
+                borderRadius: 50,
+                backgroundColor: "blue",
+              }}
+            >
+              <Text>me</Text>
+            </View>
+          </Marker>
+        )}
+      </MapView>
     </View>
   );
 }
@@ -13,8 +53,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  map: {
+    width: "100%",
+    height: "90%",
   },
 });
